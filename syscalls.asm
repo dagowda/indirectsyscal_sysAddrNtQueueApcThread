f85d214e1ce95bacbe2acf939a57820ca306551c
dhanush <114467712+dagowda@users.noreply.github.com>
2025-02-26 22:46:23 -0500

    Update syscalls.asm

--- syscalls.asm
+++ syscalls.asm
@@ -1,5 +1,6 @@
 
 
+
 EXTERN wNtAllocateVirtualMemory:DWORD               ; Extern keyword indicates that the symbol is defined in another module. Here it's the syscall number for NtAllocateVirtualMemory.
 EXTERN sysAddrNtAllocateVirtualMemory:QWORD         ; The actual address of the NtAllocateVirtualMemory syscall in ntdll.dll.
 
@@ -9,15 +10,10 @@ EXTERN sysAddrNtWriteVirtualMemory:QWORD            ; The actual address of the 
 EXTERN wNtCreateThreadEx:DWORD                      ; Syscall number for NtCreateThreadEx.
 EXTERN sysAddrNtCreateThreadEx:QWORD                ; The actual address of the NtCreateThreadEx syscall in ntdll.dll.
 
-EXTERN wNtSuspendThread:DWORD                       ; Syscall number for NtSuspendThread.
-EXTERN sysAddrNtSuspendThread:QWORD                 ; The actual address of the NtSuspendThread syscall in ntdll.dll.
-
+              
 EXTERN wNtResumeThread:DWORD                        ; Syscall number for NtResumeThread.
 EXTERN sysAddrNtResumeThread:QWORD                  ; The actual address of the NtResumeThread syscall in ntdll.dll.
 
-
-EXTERN wNtOpenProcess:DWORD                    ; Syscall number for NtSetContextThread.
-EXTERN sysAddrNtOpenProcess:QWORD              ; The actual address of the NtSetContextThread syscall in ntdll.dll.
 
 EXTERN wNtProtectVirtualMemory:DWORD                    ; Syscall number for NtSetContextThread.
 EXTERN sysAddrNtProtectVirtualMemory:QWORD              ; The actual address of the NtSetContextThread syscall in ntdll.dll.
@@ -38,23 +34,10 @@ NtAllocateVirtualMemory ENDP                        ; End of the procedure.
 NtWriteVirtualMemory PROC
     mov r10, rcx                                    ; Move the contents of rcx to r10.
     mov eax, 03ah                  ; Move the syscall number into the eax register.
-    jmp QWORD PTR [sysAddrNtQueueApcThread]     ; Jump to the actual syscall.
+    jmp QWORD PTR [sysAddrNtWriteVirtualMemory]     ; Jump to the actual syscall.
 NtWriteVirtualMemory ENDP
 
 ; Procedure for the NtCreateThreadEx syscall
-NtCreateThreadEx PROC
-    mov r10, rcx                                    ; Move the contents of rcx to r10.
-    mov eax, wNtCreateThreadEx                      ; Move the syscall number into the eax register.
-    jmp QWORD PTR [sysAddrNtCreateThreadEx]         ; Jump to the actual syscall.
-NtCreateThreadEx ENDP
-
-
-; Procedure for the NtSuspendThread syscall
-NtSuspendThread PROC
-    mov r10, rcx                                    ; Move the contents of rcx to r10.
-    mov eax, wNtSuspendThread                       ; Move the syscall number into the eax register.
-    jmp QWORD PTR [sysAddrNtSuspendThread]          ; Jump to the actual syscall.
-NtSuspendThread ENDP
 
 ; Procedure for the NtResumeThread syscall
 NtResumeThread PROC
@@ -64,13 +47,6 @@ NtResumeThread PROC
 NtResumeThread ENDP
 
 ; Procedure for the NtSetContextThread syscall
-
-
-NtOpenProcess PROC
-    mov r10, rcx                                    ; Move the contents of rcx to r10.
-    mov eax, wNtOpenProcess                    ; Move the syscall number into the eax register.
-    jmp QWORD PTR [sysAddrNtOpenProcess]       ; Jump to the actual syscall.
-NtOpenProcess ENDP
 
 NtProtectVirtualMemory PROC
     mov r10, rcx                                    ; Move the contents of rcx to r10.
